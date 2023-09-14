32b26b2959289b53d4c434e5996f2840ae5548f1
David Escudero <david89964e@gmail.com>
2023-09-14 12:48:48 -0300

    nuevos cambios

--- js/main.js
+++ js/main.js
@@ -1,30 +1,47 @@
-function storeData() {
-    // Obtener los valores de los campos
-    let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("edad").value;
+var simuladorData = [];
 
-    // Crear objeto JSON
-    let person = {
-        nombre: nombre,
-        edad: edad
+// Función para iniciar el simulador
+function iniciarSimulador() {
+    cargarSimuladorData();
+
+    // Creacion de un objeto simulado
+    var objetoSimulado = {
+        propiedad1: 'valor1',
+        propiedad2: 'valor2',
+        propiedad3: 'valor3'
     };
 
-    // Convertir objeto a cadena JSON y almacenar en localStorage
-    localStorage.setItem('person', JSON.stringify(person));
+    simuladorData.push(objetoSimulado); // Añadimos el objeto al array
+
+    // Mostramos los objetos simulados en el DOM
+    mostrarSimuladorData();
 }
 
-function retrieveData() {
-    // Recuperar cadena JSON desde localStorage
-    let storedData = localStorage.getItem('person');
-    
-    if(storedData) {
-        // Convertir cadena JSON a objeto
-        let person = JSON.parse(storedData);
-
-        // Mostrar los datos recuperados
-        let output = document.getElementById("output");
-        output.innerHTML = `Nombre: ${person.nombre} <br> Edad: ${person.edad}`;
+// Carga datos previos del simulador desde el Storage o desde JSON
+function cargarSimuladorData() {
+    // Comprobamos si existen datos almacenados en el Storage
+    if (localStorage.simuladorData) {
+        simuladorData = JSON.parse(localStorage.simuladorData); // Cargamos los datos desde el Storage
     } else {
-        alert("No hay datos almacenados.");
+        // Si no hay datos en el Storage, podemos cargarlos desde un JSON externo o crearlos manualmente
+        // simuladorData = ... // Código para cargar datos desde JSON
     }
+}
+
+// Muestra los objetos simulados en el DOM
+function mostrarSimuladorData() {
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = ''; // Limpiamos el contenido del div de salida
+
+    // Recorremos el array de simuladorData para mostrar los objetos en el DOM
+    simuladorData.forEach(function(objeto, indice) {
+        var objetoDiv = document.createElement('div');
+        objetoDiv.innerHTML = 'Objeto ' + (indice+1) + ': ' + JSON.stringify(objeto);
+        outputDiv.appendChild(objetoDiv);
+    });
+}
+
+// Guarda los datos del simulador en el Storage
+function guardarSimuladorData() {
+    localStorage.simuladorData = JSON.stringify(simuladorData);
 }
